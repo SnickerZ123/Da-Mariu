cf7d45b06e38a2b5db51ba5d0c2e2f0460ff6cba
Jerry <techjerry6@gmail.com>
2025-08-15 09:57:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
+// da mariu vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/', // custom domain Da-mariu
   plugins: [react()],
   build: {
     outDir: 'dist'
   }
 })
-
-
